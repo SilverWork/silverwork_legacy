351ff33a9bbd93b4431f7ce6ec31c8d0f493d19f
ha6oon <73981013+ha6oon@users.noreply.github.com>
2023-06-07 00:16:01 +0900

    Update concat_all.py
    안필요한거 지우기

--- data_collection/ETL/concat_all.py
+++ data_collection/ETL/concat_all.py
@@ -1,10 +1,14 @@
+'''
+etl.py를 통해 만들어진 2018, 2020, 2022년도의 노인 공공 데이터와 그에 해당하는 상세공고 데이터가 모델링에 맞게 전처리 과정을 거쳐 3개의 csv 파일로 저장됐고, 그것들을 concat()을 통해 하나의 csv로 만드는 과정.
+'''
+
 import pandas as pd
 df1 = pd.read_csv("new_jobs2018_final_check.csv", encoding='utf-8-sig')
-print(df1)
+#print(df1)
 df2 = pd.read_csv("new_jobs2020_final_check.csv", encoding='utf-8-sig')
-print(df2)
+#print(df2)
 df3 = pd.read_csv("new_jobs2022_final_check.csv", encoding='utf-8-sig')
-print(df3)
+#print(df3)
 
 df1.drop(['workPlcNm'], axis=1, inplace=True)
 df2.drop(['workPlcNm'], axis=1, inplace=True)
@@ -12,26 +16,17 @@ df3.drop(['workPlcNm'], axis=1, inplace=True)
 df1.drop(['Unnamed: 0'], axis=1, inplace=True)
 df2.drop(['Unnamed: 0'], axis=1, inplace=True)
 df3.drop(['Unnamed: 0'], axis=1, inplace=True)
-#합친 데이터 csv로 저장하기 - 인덱스 제외
-
 
 # 데이터 합치기 - 데이터프레임 변수 : data
 df_list = [df1, df2, df3]
 df = pd.concat(df_list, ignore_index = True )
+#print(df)
 
-print(df)
-
-#합친 데이터 csv로 저장하기- snowflake용도로 index=False
+#합친 데이터 csv로 저장하기-> snowflake에 벌크 업데이트할 용도로 index=False, float_format='%.0f' 처리함
 df.to_csv('Jobs.csv', index=False, float_format='%.0f', encoding='utf-8-sig')
-print(df)
-'''
-columns = ['acptMthd','deadline','emplymShp','emplymShpNm','startDd','pk','jobcls','jobclsNm','oranNm','organYn','recrtTitle','stmId','stmNm','endDd','workPlc','acptMthdCd','age','ageYn','clerk','clerkContt','clltPrnnum','createDy','detCnts','etcItm','homepage','plDetAddr','plbizNm','updDt']
-df  = df[columns]
-# 합친 데이터 잘 저장됐는지 체크하기
-data_modified = pd.read_csv("Jobs.csv",encoding='utf-8-sig')
-data_modified.tail()
-'''
+#print(df)
 
-df4 = pd.read_csv("Jobs.csv", encoding='utf-8-sig')
-print(df4)
-print(len(df4))
+# 합친 데이터 잘 저장됐는지 체크하기
+#df4 = pd.read_csv("Jobs.csv", encoding='utf-8-sig')
+#print(df4)
+#print(len(df4))
